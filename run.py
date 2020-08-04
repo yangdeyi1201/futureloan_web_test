7664316c7530a3e6f00056856f73bf20022a5498
yangdeyi <yangdeyi1201@foxmail.com>
2020-08-05 02:11:38 +0800

    0805_morning_v2

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     allure_raw_path = REPORTS_PATH/'allure-raw'
 
     # 执行脚本生成 allure 报告时，先判断删除之前的 allure-raw 源文件
-    if Path.exists(allure_raw_path):
-        shutil.rmtree(allure_raw_path)
+    # if Path.exists(allure_raw_path):
+    #     shutil.rmtree(allure_raw_path)
     # 再执行脚本生成 allure 报告，以确保 allure 报告最新
     # pytest.main(['-m', 'not expired', '--reruns', '3', '--reruns-delay', '5', '--alluredir=reports/allure-raw', '-s'])
     pytest.main(['-m', 'login', '-s'])
